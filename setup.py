2f44763519c3da47b55d6b9776a08e016c3f6a73
Alex Ehlke <alex.ehlke@gmail.com>
2011-04-08 01:53:47 -0400

    added all the files over from the manabi app

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+import os
+
+setup(
+    name='django-kanjivg',
+    description='KanjiVG support lib for Manabi.',
+
+    author='Alex Ehlke',
+    author_email='alex.ehlke@gmail.com',
+    url='http://github.com/aehlke/kanjivg',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Django',
+        'lxml',
+    ],
+)
+
